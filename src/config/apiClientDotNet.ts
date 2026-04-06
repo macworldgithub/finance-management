d1ebb6897f7b52d0c3a3a09cae466a82c70ac9f0
Syed Jawwad Kareem Naqvi <139786195+jawwadkareem@users.noreply.github.com>
2026-04-06 22:54:58 +0500

    Update API base URL for DotNet client

--- src/config/apiClientDotNet.ts
+++ src/config/apiClientDotNet.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_DOTNET_BASE_URL = "https://financedotnet.omnisuiteai.com/api";
+const API_DOTNET_BASE_URL = "https://backend-finance.omnisuiteai.com/api";
 
 export const apiClientDotNet = axios.create({
   baseURL: API_DOTNET_BASE_URL,
